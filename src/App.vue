570fcc955aad5478e86f376fea7ca31713b84a61
Martin Burchard <dev@burchard-it.de>
2020-10-01 10:31:03 +0200

    removed HelloWorld.vue, removed vue-class-component
    Vue class component seems to be no longer supported. usage leads to problems in IntelliJ.
    https://scotch.io/bar-talk/class-components-in-vue-are-no-longer-happening

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import {Options, Vue} from 'vue-class-component'
-import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent} from 'vue'
 
-@Options({
-  components: {
-    HelloWorld,
-  },
+export default defineComponent({
+  name: 'App',
+  components: {},
 })
-export default class App extends Vue {}
 </script>
 
 <style lang="scss">
